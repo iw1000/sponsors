c2151a6fb98e99432c282d8f9c5ba4a480585cfc
Lucas Cheon <lucascheon@Lucass-MacBook-Pro.local>
2021-07-12 15:37:08 +0900

    ssimple ui

--- nft/scripts/simple_collectible/create_collectible.py
+++ nft/scripts/simple_collectible/create_collectible.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/opt/homebrew/bin/python3
 from brownie import SimpleCollectible, accounts, network, config
 from scripts.helpful_scripts import OPENSEA_FORMAT
 
-sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+# sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+sample_token_uri = "https://ipfs.io/ipfs/QmdT2h4K7hkWVjrKPwBUm6ubejgd3LD53qBLTcqjqQFumb?filename=LUCAS.json"
 
 def main():
     dev = accounts.add(config["wallets"]["from_key"])
--- nft/scripts/simple_collectible/deploy_simple.py
+++ nft/scripts/simple_collectible/deploy_simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/opt/homebrew/bin/python3
 import os
 from brownie import SimpleCollectible, accounts, network, config
 from dotenv import load_dotenv
